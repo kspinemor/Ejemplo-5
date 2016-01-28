afcd9e4f14ffdf08348ae7acdba8027b7c057348
Kevin Stiven Pineda Morales <kevinpinedamyg@gmail.com>
2016-01-28 11:26:04 -0500

    Ejercicio 3

---
+++ Ejercicio_3/src/main/java/com/mycompany/ejercicio_3/Ejercicio3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicio_3;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Holis :v"); // TODO code application logic here
+    }
+    
+}
