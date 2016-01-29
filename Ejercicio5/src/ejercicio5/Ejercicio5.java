8708574b6291e3167f49cd2deff6160f4fadcc1f
Kevin Stiven Pineda Morales <kevinpinedamyg@gmail.com>
2016-01-29 11:02:37 -0500

    Listo primera version 1.1

--- Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -15,7 +15,8 @@ public class Ejercicio5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Holis :v");    
+        System.out.println("Holis :v jajajaj Perras .-."
+                + "");    
     }
     
 }
