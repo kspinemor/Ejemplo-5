bd51fa82ac6940c37a88ed6caa8df3776a71618c
Kevin Stiven Pineda Morales <kevinpinedamyg@gmail.com>
2016-02-03 08:47:35 +0100

    Merge commit '0df128214b4a81ba06dcaebe3c4bc048c48aa62d'
    Conflicts:
    	Ejercicio5/src/ejercicio5/Ejercicio5.java

--- Ejercicio_3/src/main/java/com/mycompany/ejercicio_3/Ejercicio3.java
+++ Ejercicio_3/src/main/java/com/mycompany/ejercicio_3/Ejercicio3.java
@@ -15,7 +15,7 @@ public class Ejercicio3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Holis :v"); // TODO code application logic here
+        System.out.println("Holisdasd :v"); // TODO code application logic here
     }
     
 }
